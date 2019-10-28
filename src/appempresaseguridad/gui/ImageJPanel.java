3a59f4fc772d27889c67abe052a3fc2ecccacf77
Alejandro Rueda Duarte <alejrudu@suramericana.com.co>
2019-10-28 14:38:38 -0500

    commit inicial del proyecto

---
+++ src/appempresaseguridad/gui/ImageJPanel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appempresaseguridad.gui;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class ImageJPanel extends JPanel {
+
+    private Image fondo=null;
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(fondo,0,0,getWidth(),getHeight(),null);
+    }
+    public void setImage(String image){
+        if (image!=null) {
+            fondo=new ImageIcon(getClass().getResource(image)).getImage();
+        }
+    }
+
+}
